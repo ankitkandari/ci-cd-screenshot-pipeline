b3fe16fdbad6199ff28da87bfdb1b1e18a2c9c69
Ankit Singh <codertacina@gmail.com>
2023-07-03 23:19:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import peace from './assets/peace.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload4.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={peace} />
+      <h1>Welcome to Awesome Homepage</h1>
     </div>
   );
 }
